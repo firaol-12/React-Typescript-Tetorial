33d1710ac3b2f1d1bfa4d3fdab51edbf2ba8a751
Firaol Negash <feranegash604@gmail.com>
2026-07-16 10:17:10 +0300

    compnent

---
+++ react tetorial/src/App.tsx
@@ -0,0 +1,11 @@
+import Try from './components/try'
+import './App.css'
+
+export default function App() {
+  return (
+    <>
+      <Try />
+    </>
+  )
+}
+
---
+++ react tetorial/src/components/try.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Try() {
+    const name : string = "Fira";
+    
+    return(        
+        <div>
+            <h1>Home {name}</h1>
+        </div>
+    )
+}

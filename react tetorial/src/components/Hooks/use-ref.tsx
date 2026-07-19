02c87682125c3b2ea3561cf0b4905e0643bbd1c9
Firaol Negash <feranegash604@gmail.com>
2026-07-19 11:02:06 +0300

    practice 2 on useRef to make the input feild show after submit btn clicked by addtionaly using useState

--- react tetorial/src/components/Hooks/use-ref.tsx
+++ react tetorial/src/components/Hooks/use-ref.tsx
@@ -1,12 +1,23 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef , useState} from "react";
 
 export default function UseRef(){
     let inputRef = useRef<HTMLInputElement>(null);
+    let [input, setInput] = useState(null)
+
+    function hundelClick(){
+        inputRef.current?.focus();
+        setInput(inputRef.current.value)
+        console.log(input);
+    }
 
     useEffect(()=>{
         inputRef.current?.focus();
     },[]);
     return(
+        <div>
+        <h1>{input}</h1>
         <input ref={inputRef} placeholder="Type here" />
+        <button onClick={hundelClick}>Sumbmit</button>
+        </div>
     )
 }
